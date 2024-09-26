f4b38711dd7b7042d3bb1aa170c79b44646e29ee
neezc <123064186+OreLabs-Ai@users.noreply.github.com>
2024-09-26 21:52:15 +0700

    Delete seed directory

--- seed/bot/utils/logger.py
+++
@@ -1,8 +0,0 @@
-import sys
-from loguru import logger
-
-
-logger.remove()
-logger.add(sink=sys.stdout, format="<blue>[</blue><white>+</white><yellow>]</yellow>"
-                                   " ➤  <white><b>{message}</b></white>")
-logger = logger.opt(colors=True)
